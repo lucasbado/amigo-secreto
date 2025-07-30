a6fd7591b0d2a081e6f99461498da9b36416dfac
lucasbado <luc.bado@gmail.com>
2025-07-29 21:35:19 -0300

    Add: funcionalidade para apagar a lista de amigos e validação de entrada

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ function exibirTextoNaTela(tag, texto) {
     campo.innerHTML = texto;
 }
 
+
+
 function exibirNomeAdicionado() {
     let amigo = document.querySelector('input').value;
     exibirTextoNaTela('p', `Amigo(a) ${amigo} adicionado com sucesso!`);
     setTimeout(() => {
         exibirTextoNaTela('p', `Amigo(a)s adicionados: ${listaAmigos.length}`);
     }, 3000);
-    
+
 }
 
 function adicionarNaLista() {
@@ -23,42 +25,56 @@ function adicionarNaLista() {
     let item = document.createElement('li');
     item.textContent = nomeAmigo;
     lista.appendChild(item);
-    
+
 }
+
+
 
 
 function adicionarAmigo() {
     let amigo = document.querySelector('input').value;
+
+    if (amigo.trim() === '') {
+        exibirTextoNaTela('p', 'Por favor, digite um nome válido.');
+        return;
+    }
     listaAmigos.push(amigo);
     exibirNomeAdicionado();
     adicionarNaLista();
     limparCampo();
-    console.log(listaAmigos);
-    
-
+    document.getElementById('listaAmigos').style.display = 'block';
 }
 
 function sortearAmigo() {
 
-   if (listaAmigos.length === 0) {
+    if (listaAmigos.length === 0) {
         alert("Adicione amigos antes de sortear.");
         return;
     }
     let resultado = document.getElementById('resultado');
     let indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
     let nomeSorteado = listaAmigos[indiceAleatorio];
-    console.log("Lista de amigos:", indiceAleatorio);
+    document.getElementById('listaAmigos').style.display = 'none';
     resultado.innerHTML += `O amigo(a) sorteado(a) foi <li>${nomeSorteado}</li>`;
-  
     
-
-    
-
-    
+    setTimeout(() => {
+        resultado.innerHTML = '';
+    }, 2000);
     
 }
 
+function apagarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    listaAmigos = [];
+    exibirTextoNaTela('p', 'Lista de amigos apagada com sucesso!');
+    setTimeout(() => {
+        exibirTextoNaTela('p', `Amigo(a)s adicionados: ${listaAmigos.length}`);
+    }, 1000);
+    
+    
 
+}
 
 
 function limparCampo() {
@@ -67,3 +83,8 @@ function limparCampo() {
 
 }
 
+document.getElementById('amigo').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        adicionarAmigo();
+    }
+});
